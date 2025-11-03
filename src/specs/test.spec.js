15adad80a7f4d0fdfbf7513ec8a68410afbd39e1
Vitalij <vitalijsnopov@gmail.com>
2025-11-03 14:01:02 +0200

    feat: add additional test cases for hamburger menu and Services page navigation

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -7,4 +7,17 @@ describe('first e2e test suite', () => {
         await expect(browser).toHaveTitle(expectedTitle);
 
     });
+    it('should open hamburger menu', async () => {
+        await browser.url('/');
+        await $('.hamburger-menu__button').click();
+        await expect($("//div[@class='os-content']")).toBeDisplayed();
+
+    });
+    it('should navigate to Services page', async () => {
+        await browser.url('/');
+        const links = await $$('.top-navigation__item-link')
+        await links[0].click();
+        await expect(browser).toHaveTitle('Services | EPAM')
+    });
 });
+
