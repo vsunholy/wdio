c66552d54b8151a8f4812dbd9152caf2843e2e52
Vitalij <vitalijsnopov@gmail.com>
2025-11-03 14:42:57 +0200

    feat: update geckodriver version and add Firefox capability to WebdriverIO configuration

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,6 +51,8 @@ export const config = {
     //
     capabilities: [{
         browserName: 'chrome'
+    },{
+        browserName: 'firefox'
     }],
 
     //
@@ -100,7 +102,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    // services: ['geckodriver'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
