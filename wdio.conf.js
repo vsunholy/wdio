5b64e33dfe2f587a237b3b32d36c13eb5a9d5b6b
Vitalij <vitalijsnopov@gmail.com>
2025-11-04 09:37:51 +0200

    feat: add JUnit reporter and update dependencies in configuration files

--- wdio.conf.js
+++ wdio.conf.js
@@ -129,7 +129,12 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec', ['junit', {
+        outputDir: "./report",
+        outputFileFormat: function (options) {
+            return `results-${options.cid}.xml`;
+        },
+    }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
